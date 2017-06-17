3ebeee47770bbb961b069d2c660553951bdbc888
Alex Huang <alexhuang@Alexs-MBP.hsd1.ca.comcast.net>
2017-06-16 19:58:57 -0700

    Added bar plot function that groups by 2 categorical variables. Changed names of arguments.

--- plot.py
+++ plot.py
@@ -3,6 +3,16 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+def _index_to_name(df, col):
+    if isinstance(col, int):
+        col = df.columns[col]
+    return col
+
+def _top_n_cat(a, n=5):
+    counts = a.value_counts(dropna=False)
+    top = counts.iloc[:n].index
+    return a.apply(lambda x: x if x in top else 'other')
+
 def plot_missing(df, n=5):
     a = df.isnull().mean(axis=0)
     a = a[a > 0]
@@ -15,6 +25,7 @@ def plot_missing(df, n=5):
 
     df.xlabel('proportions')
 
+# col should be categorical
 def plot_bar(df, col=None, prop=True, **kwargs):
     if col:
         col = _index_to_name(df, col)
@@ -36,16 +47,43 @@ def plot_bar(df, col=None, prop=True, **kwargs):
     plt.xlabel(xlabel)
     plt.title(col)
 
-def plot_grouped_bar(df, col1, col2, stacked=False):
+# col should be contiuous or binary
+def plot_grouped_bar(df, cat, col, stacked=False):
     df = df.copy()
 
-    col1 = _index_to_name(df, col1)
-    col2 = _index_to_name(df, col2)
+    cat = _index_to_name(df, cat)
+    col = _index_to_name(df, col)
 
-    df[col1] = _top_n_cat(df[col1])
-    df[col2] = _top_n_cat(df[col2])
+    df[cat] = _top_n_cat(df[cat])
 
-    df.groupby(col1)[col2].mean().sort_index(ascending=False).plot.barh(stacked=stacked)
+    df.groupby(cat)[col].mean().sort_index(ascending=False).plot.barh(stacked=stacked)
 
     plt.xlabel('proportions')
-    plt.title('%s vs. %s' % (col1, col2))
+    plt.title('%s vs. %s' % (cat, col))
+
+# col should be continuous or binary
+def plot_grouped_bar2(df, cat1, cat2, col, stacked=False):
+    df = df.copy()
+
+    cat1 = _index_to_name(df, cat1)
+    cat2 = _index_to_name(df, cat2)
+
+    df[cat1] = _top_n_cat(df[cat1])
+    df[cat2] = _top_n_cat(df[cat2])
+
+    fig, ax = plt.subplots()
+
+    if stacked:
+        pd.crosstab(df[cat1], df[cat2], df[col], aggfunc=np.mean)\
+            .sort_index(axis=0, ascending=False)\
+            .plot.barh(ax=ax, stacked=True, color=reversed(plt.rcParams['axes.color_cycle'][:5]))
+
+    else:
+        pd.crosstab(df[cat1], df[cat2], df[col], aggfunc=np.mean)\
+            .sort_index(axis=0, ascending=False)\
+            .sort_index(axis=1, ascending=False)\
+            .plot.barh(ax=ax, legend='reverse')
+
+    plt.legend(title=cat2, loc=(1, 0.5))
+    plt.xlabel(col)
+    plt.title('%s vs. %s' % (cat1, cat2))
