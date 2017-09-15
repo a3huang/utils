275269266b5032d6f552b96442af8b6902c363f1
Alex Huang <alexhuang861@gmail.com>
2017-09-15 16:33:41 -0700

    Updated docstrings

--- plot.py
+++ plot.py
@@ -3,27 +3,25 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix, roc_curve, f1_score
 from sklearn.model_selection import learning_curve, StratifiedKFold
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
-from rpy2.robjects import pandas2ri, r
-from sklearn import tree
-import itertools
+import os
 import pydotplus
 import subprocess
 
+# remove later?
 from utils.data import table, rename
-
-import os
+from rpy2.robjects import pandas2ri, r
 
 #
 def dummy_categorical(df, n):
     '''
-    Create a categorical column with labels 1 to n for testing purposes.
+    Creates a categorical column with labels 1 to n for testing purposes.
 
     ex) df['dummy'] = df.pipe(dummy_categorical, 5)
     '''
@@ -42,7 +40,7 @@ def dummy_categorical(df, n):
 #
 def dummy_continuous(df, loc=0, scale=1):
     '''
-    Create a continuous column from a normal distribution for testing purposes.
+    Creates a continuous column from a normal distribution for testing purposes.
 
     ex) df['dummy'] = df.pipe(dummy_continuous)
     '''
@@ -62,7 +60,7 @@ def facet(df, row, col, **kwargs):
 #
 def ceil_with_base(x, base):
     '''
-    Take the ceiling of a number with respect to any base.
+    Takes the ceiling of a number with respect to any base.
 
     ex) ceil_with_base(0.03, 0.05) -> 0.05
     ex) ceil_with_base(0.02, 0.05) -> 0.05
@@ -73,7 +71,7 @@ def ceil_with_base(x, base):
 #
 def round_with_base(x, base):
     '''
-    Round a number with respect to any base.
+    Rounds a number with respect to any base.
 
     ex) round_with_base(0.03, 0.05) -> 0.05
     ex) round_with_base(0.02, 0.05) -> 0
@@ -84,7 +82,7 @@ def round_with_base(x, base):
 #
 def nice_round(x):
     '''
-    Round a number "nicely" to the nearest denomination of 5 or 10. Numbers less
+    Rounds a number "nicely" to the nearest denomination of 5 or 10. Numbers less
     than 1 will be always be rounded upwards. Numbers greater than 1 may be rounded
     upwards or downwards.
 
@@ -146,7 +144,7 @@ def nice_range_bin(ax, range=None):
 #
 def take(iterator, n):
     '''
-    Return the nth item in an iterator.
+    Returns the nth item in an iterator.
 
     ex) take(df.groupby('Generation'), 2)
     '''
@@ -224,7 +222,7 @@ def prop_hist(a, prop=False, **kwargs):
 #
 def barplot(df, col, by=None, kind=None, prop=False):
     '''
-    Create a bar plot for a categorical variable. Group by an optional 2nd
+    Creates a bar plot for a categorical variable. Group by an optional 2nd
     categorical variable.
 
     ex) df.pipe(barplot, col='HP', by='Type')
@@ -264,7 +262,7 @@ def barplot(df, col, by=None, kind=None, prop=False):
 #
 def boxplot(df, col, by, facet_by=None, sort_median=False):
     '''
-    Create a grouped box plot for a continuous variable. Facet by an optional
+    Creates a grouped box plot for a continuous variable. Facet by an optional
     3rd categorical variable.
 
     ex) df.pipe(boxplot, col='HP', by='Type')
@@ -285,7 +283,7 @@ def boxplot(df, col, by, facet_by=None, sort_median=False):
 #
 def distplot(df, col, by=None, prop=False, facet=False, range=None):
     '''
-    Create a histogram for a continuous variable. Group by an optional 2nd
+    Creates a histogram for a continuous variable. Group by an optional 2nd
     categorical variable.
 
     ex) df.pipe(distplot, col='HP', by='Type')
@@ -312,7 +310,7 @@ def distplot(df, col, by=None, prop=False, facet=False, range=None):
 #
 def heatplot(df, x, y, z=None, normalize=False):
     '''
-    Create a heatmap between 2 categorical variables. Calculate the mean for an
+    Creates a heatmap between 2 categorical variables. Calculate the mean for an
     optional 3rd continuous variable.
 
     ex) df.pipe(heatplot, x='Type 1', y='Type 2', z='Attack')
@@ -326,7 +324,7 @@ def heatplot(df, x, y, z=None, normalize=False):
 #
 def scatplot(df, x, y, by=None, facet=False):
     '''
-    Create a scatter plot for 2 continuous variables. Group by an optional 3rd
+    Creates a scatter plot for 2 continuous variables. Group by an optional 3rd
     categorical variable.
 
     ex) df.pipe(scatplot, x='Attack', y='Defense', by='Legendary')
@@ -345,7 +343,7 @@ def scatplot(df, x, y, by=None, facet=False):
 #
 def interactplot(df, col, by, val, heat=False):
     '''
-    Create an interaction lineplot or heatmap between 2 predictor variables and
+    Creates an interaction lineplot or heatmap between 2 predictor variables and
     a 3rd target variable.
 
     ex) df.pipe(interactplot, col='Type 1', by='Type 2', val='Attack')
@@ -362,7 +360,7 @@ def interactplot(df, col, by, val, heat=False):
 #
 def tsplot(df, date, by=None, val=None, freq='M', area=False):
     '''
-    Create a time series plot of counts for a date variable or of mean values
+    Creates a time series plot of counts for a date variable or of mean values
     for a continuous variable. Group by an optional 3rd categorical variable.
 
     ex) df.pipe(tsplot, date='date', by='Category')
@@ -388,7 +386,7 @@ def tsplot(df, date, by=None, val=None, freq='M', area=False):
 #
 def tsboxplot(df, date, col, freq='M'):
     '''
-    Create a time series boxplot for a continuous variable.
+    Creates a time series boxplot for a continuous variable.
 
     ex) df.pipe(tsboxplot, date='date', col='Amount')
     '''
@@ -407,7 +405,7 @@ def tsboxplot(df, date, col, freq='M'):
 #
 def genboxplot(df, by, folder_name, default_dir='/Users/alexhuang/'):
     '''
-    Generate boxplots for each column grouped by a fixed categorical variable
+    Generates boxplots for each column grouped by a fixed categorical variable
     and save them to a folder.
 
     ex) df.pipe(genboxplot, by='Target', folder_name='plots')
@@ -420,162 +418,137 @@ def genboxplot(df, by, folder_name, default_dir='/Users/alexhuang/'):
     df = df.copy()
     df = df.select_dtypes(include=[np.number])
 
-    for i, col in enumerate(df.columns.difference([by])):
+    for i, col in enumerate(df.drop(by, 1)):
         sns.boxplot(df[col], df[by], orient='h')
         plt.xlabel('')
         plt.title(col)
         plt.savefig(directory + '%s.png' % i)
         plt.close()
         print 'Saved Plot: %s' % col
-######
 
-def plot_pca(df, cat, pca_model=None, sample_size=1000, **kwargs):
+#
+def pcaplot(df, by, method=None, sample_size=None):
+    '''
+    Creates a 2-D scatterplot of the entire dataset grouped by a categorical
+    variable. Uses PCA method by default for dimensionality reduction.
+
+    ex) df.pipe(pcaplot, by='Legendary')
+    '''
+
     df = df.copy()
-    s = StandardScaler()
 
-    if pca_model is None:
-        pca_model = PCA()
+    if method is None:
+        method = PCA()
+
+    pipeline = make_pipeline(StandardScaler(), method)
 
     if sample_size:
         df = df.sample(sample_size)
 
-    X = df[df.columns.difference([cat])]
-    df['PCA 1'] = pca_model.fit_transform(s.fit_transform(X))[:, 0]
-    df['PCA 2'] = pca_model.fit_transform(s.fit_transform(X))[:, 1]
-    plot_scatter(df, cat, 'PCA 1', 'PCA 2', fit_reg=False, **kwargs)
-    return df
+    X = df.drop(by, 1)
+    df['PCA 1'] = pipeline.fit_transform(X)[:, 0]
+    df['PCA 2'] = pipeline.fit_transform(X)[:, 1]
+    df.pipe(scatplot, x='PCA 1', y='PCA 2', by=by)
 
-def plot_clusters(df, cluster_model=None, pca_model=None, sample_size=1000, **kwargs):
-    df = df.copy()
-    s = StandardScaler()
+#
+def plot_f1_scores(model, X, y):
+    '''
+    For each threshold value, calculates the mean 5-fold CV f1 score. Creates
+    a lineplot of the threshold values vs. the f1 scores.
 
-    if cluster_model is None:
-        cluster_model = KMeans()
+    ex) plot_f1_scores(model, X_train, y_train)
+    '''
 
-    if sample_size:
-        df = df.sample(sample_size)
+    cv = StratifiedKFold(n_splits=5, shuffle=True)
 
-    cluster_model.fit(s.fit_transform(df))
-    df['cluster'] = cluster_model.labels_
-    plot_pca(df, 'cluster', pca_model, sample_size=None, **kwargs)
-    return df
+    cv_scores = []
+    for threshold in np.linspace(.1, 1, 10):
 
-def plot_decision_tree(df, X, y, filename, **kwargs):
-    #X = df[df.columns.difference([target])]
-    #y = df[target]
+        inner_cv_scores = []
+        for train, test in cv.split(X, y):
+            true = y.values[test]
+            pred = model.predict_proba(X.values[test])[:, 1] > threshold
+            inner_cv_scores.append(f1_score(true, pred))
 
-    model = tree.DecisionTreeClassifier(**kwargs)
-    model.fit(X, y)
-    dot_data = tree.export_graphviz(model,
-                                    out_file=None,
-                                    feature_names=X.columns,
-                                    filled=True,
-                                    class_names=y.astype(str).unique())
-    graph = pydotplus.graph_from_dot_data(dot_data)
-    graph.write_pdf(filename)
-    subprocess.call(('open', filename))
-    return model
+        cv.scores.append(np.mean(inner_cv_scores))
 
-def plot_feature_importances(model, X, top=None, **kwargs):
-    #X = df[df.columns.difference([target])]
-    a = _get_feature_importances(model, X)
-    model_name = _get_model_name(model)
+    plt.plot(np.linspace(.1, 1, 10), cv_scores)
 
-    if top:
-        a = a[:top]
+#
+def plot_confusion_matrix(model, X, y, threshold=0.5, normalize=False):
+    '''
+    Creates a heatmap of the confusion matrix for the given model and data.
 
-    a.sort_index(ascending=False).set_index(0).plot.barh(**kwargs)
-    plt.ylabel('Feature')
-    plt.title(model_name)
-    plt.legend().remove()
-    return a
-
-def plot_feature_scores(df, scores, top=5):
-    pd.DataFrame(sorted(zip(df.columns, scores), key=lambda x: x[1], reverse=True)[:top])\
-        .set_index(0).sort_values(by=1).plot.barh()
-
-def plot_confusion_matrix(model, X, y, threshold=0.5, norm_axis=1, **kwargs):
-    # X = df.drop(target, 1)
-    # y = df[target]
+    ex) plot_confusion_matrix(model, X_test, y_test, normalize='index')
+    '''
 
     try:
-        prediction = model.predict_proba(X)[:, 1] > threshold
+        pred = model.predict_proba(X)[:, 1] > threshold
     except:
-        prediction = model.predict(X)
+        pred = model.predict(X)
 
-    a = confusion_matrix(y, prediction).astype(float)
-    a = np.divide(a, np.sum(a, norm_axis))
-    sns.heatmap(a, annot=True, fmt='.2f', **kwargs)
+    a = confusion_matrix(y, pred).astype(float)
+
+    if normalize == 'index':
+        a = np.divide(a, np.sum(a, 1).reshape(2, 1))
+    elif normalize == 'column':
+        a = np.divide(a, np.sum(a, 0))
+
+    sns.heatmap(a, annot=True, fmt='.2f')
     plt.ylabel('True')
     plt.title('Predicted')
-    return a
 
-def plot_roc_curve(model, X, y):
-    # X = df.drop(target, 1)
-    # y = df[target]
+#
+def plot_roc_curves(model, X, y):
+    '''
+    Creates a lineplot of the roc curve for the given model and data.
 
-    #model_name = _get_model_name(model)
+    ex) plot_roc_curves(model, X_test, y_test)
+    '''
 
     try:
-        prediction = model.predict_proba(X)[:, 1]
+        pred = model.predict_proba(X)[:, 1]
     except:
-        prediction = model.predict(X)
+        pred = model.predict(X)
 
-    fpr, tpr, _ = roc_curve(y, prediction)
+    fpr, tpr, _ = roc_curve(y, pred)
     plt.plot(fpr, tpr)
     plt.plot([0, 1], [0, 1], linestyle='--')
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title(model_name)
 
+#
 def plot_learning_curves(model, X, y):
-    # X = df.drop(target, 1)
-    # y = df[target]
+    '''
+    For each sample size, splits the given data into 5 CV train test pairs.
+    Calulates the mean CV score over the 5 training sets and the mean CV score
+    over the 5 validation sets. Creates a lineplot of the sample sizes vs.
+    the mean CV train set scores and the mean CV validation set scores.
+
+    ex) plot_learning_curves(model, X_train, y_train)
+    '''
 
     cv = StratifiedKFold(n_splits=5, shuffle=True)
-
-    model_name = _get_model_name(model)
-
     sizes, train, validation = learning_curve(model, X, y, cv=cv, scoring='roc_auc')
     plt.plot(sizes, np.mean(train, axis=1), label='train')
     plt.plot(sizes, np.mean(validation, axis=1), label='validation')
     plt.xlabel('Sample Size')
     plt.ylabel('Performance')
-    plt.title(model_name)
-    plt.legend(loc=(1, 0.5))
+    plt.legend(loc=(1, 0))
+######
 
-def plot_prob_estimates(df, model):
-    model_name = _get_model_name(model)
+def plot_decision_tree(df, X, y, filename, **kwargs):
+    #X = df[df.columns.difference([target])]
+    #y = df[target]
 
-    plt.hist(model.predict_proba(df)[:, 1])
-    plt.label('Probability')
-    plt.title(model_name)
-
-def plot_probs(model, X_test, **kwargs):
-    plt.hist(model.predict_proba(X_test)[:, 1], **kwargs)
-
-def plot_word_frequencies(docs, top=20, **kwargs):
-    c = CountVectorizer()
-    c.fit(docs)
-
-    counts = c.fit_transform(docs).sum(axis=0)
-    counts = np.array(counts).squeeze()
-
-    vocab = pd.DataFrame([(k, counts[v]) for k, v in c.vocabulary_.items()], columns=['Word', 'Count'])
-    total = vocab['Count'].sum()
-    vocab['Frequency'] = vocab['Count'] / total
-    vocab = vocab.sort_values(by='Count', ascending=False).set_index('Word')
-
-    if top:
-        vocab = vocab[:top]
-
-    vocab[['Frequency']].sort_values(by='Frequency').plot.barh(**kwargs)
-
-    plt.xlabel('Proportion')
-    plt.ylabel('Words')
-    plt.legend().remove()
-
-    return vocab
+    model = DecisionTreeClassifier(**kwargs)
+    model.fit(X, y)
+    dot_data = export_graphviz(model, out_file=None, feature_names=X.columns,
+                               filled=True, class_names=y.astype(str).unique())
+    graph = pydotplus.graph_from_dot_data(dot_data)
+    graph.write_pdf(filename)
+    subprocess.call(('open', filename))
+    return model
 
 def loess(df, col1, col2):
     pandas2ri.activate()
@@ -606,27 +579,3 @@ def compare_feature_sets_bar(a, b, col):
 
     pd.concat([a, b], axis=1)[[col]].plot.bar()
     plt.legend(loc=(1,.5))
-
-def plot_f1(model, X, y):
-    cv = StratifiedKFold(n_splits=5, shuffle=True)
-
-    l = []
-    for i in np.linspace(.1,1,10):
-        l1 = []
-        for tr, te in cv.split(X, y):
-            true = y.values[te]
-            pred = model.predict_proba(X.values[te])[:, 1] > i
-            l1.append(f1_score(true, pred))
-        l.append(np.mean(l1))
-
-    plt.plot(np.linspace(.1,1,10), l)
-
-def plot_true_pred(model, X_test, y_test):
-    xlims = model.predict(X_test).min(), model.predict(X_test).max()
-    ylims = y_test.min(), y_test.max()
-    range_min = min(xlims[0], ylims[0])
-    range_max = max(xlims[1], ylims[1])
-    plt.scatter(y_test, model.predict(X_test))
-    plt.plot([range_min, range_max], [range_min, range_max], linestyle='--')
-    plt.xlabel('True Values')
-    plt.ylabel('Predictions')
