e67b1ca5e80449783d939fc5d9113614a77d4305
Alex Huang <alexhuang861@gmail.com>
2018-05-30 17:02:32 -0700

    WIP added more requests

--- gs_utils.py
+++ gs_utils.py
@@ -200,3 +200,128 @@ def write_df_to_cell(gc, spreadsheet_id, grid_range, df):
     gc.service.spreadsheets().values()\
         .update(spreadsheetId=spreadsheet_id, range=grid_range,
                 valueInputOption='USER_ENTERED', body=body).execute()
+
+
+def create_new_sheet(client, name):
+    existing = [i['name'] for i in client.list_ssheets()
+                if i['name'] == name]
+    if len(existing) == 0:
+        return client.create(name)
+    else:
+        raise Exception
+
+
+def paste_df_in_google_sheet(spreadsheet, df, sheet='Sheet1', cell='A1'):
+    s = spreadsheet.worksheet_by_title(sheet)
+    s.set_dataframe(df.astype(str), cell)
+
+
+def get_freeze_request(sheet_id, num_rows, num_cols):
+    request = {
+        'updateSheetProperties': {
+             'fields': 'gridProperties(frozenRowCount, frozenColumnCount)',
+             'properties': {
+                'gridProperties': {
+                    'frozenRowCount': num_rows,
+                    'frozenColumnCount': num_cols
+                },
+                'sheetId': sheet_id
+             }
+        }
+    }
+    return request
+
+
+def get_filter_request(sheet_id):
+    request = {
+        'setBasicFilter': {
+            'filter': {
+                'range': {
+                    'sheetId': sheet_id,
+                    'startColumnIndex': 0
+                }
+            }
+        }
+    }
+    return request
+
+
+def get_color_dict():
+    d = {'grey': {'red': 50, 'green': 50, 'blue': 50},
+         'green': {'red': 72, 'green': 30, 'blue': 50},
+         'red': {'red': 11, 'green': 56, 'blue': 60},
+         'yellow': {'red': 3, 'green': 23, 'blue': 77}
+         }
+    return d
+
+
+def get_color_range_request(sheet_id, start_row=None, end_row=None,
+                            start_col=None, end_col=None, color='grey'):
+    c = get_color_dict()[color]
+
+    request = {
+      'repeatCell': {
+        'range': {
+          'sheetId': sheet_id,
+          'startRowIndex': start_row,
+          'endRowIndex': end_row,
+          'startColumnIndex': start_col,
+          'endColumnIndex': end_col
+        },
+        'cell': {
+          'userEnteredFormat': {
+            'backgroundColor': c,
+          }
+        },
+        'fields': 'userEnteredFormat(backgroundColor)'
+      }
+    }
+    return request
+
+
+def get_resize_request(sheet_id, start, end):
+    request = {
+      'autoResizeDimensions': {
+        'dimensions': {
+          'sheetId': sheet_id,
+          'dimension': 'COLUMNS',
+          'startIndex': start,
+          'endIndex': end
+        }
+      }
+    }
+    return request
+
+
+def create_value_object(x):
+    return {'userEnteredValue': str(x)}
+
+
+def get_conditional_formatting_request(sheet_id, start, end, type,
+                                       values, color):
+    c = get_color_dict()[color]
+
+    request = {
+      'addConditionalFormatRule': {
+        'rule': {
+          'ranges': [
+            {
+              'sheetId': sheet_id,
+              'startColumnIndex': start,
+              'endColumnIndex': end,
+            }
+          ],
+          'booleanRule': {
+            'condition': {
+              'type': type,
+              'values': [create_value_object(i) for i in values]
+            },
+            'format': {
+              'backgroundColor': c
+            }
+          }
+        },
+        'index': 0
+      }
+    }
+    return request
